08949f243af93c0a2730902710f62d8b26ab7475
Sebastian Meadows <snhmeadows@outlook.com>
2023-09-20 22:09:05 -0500

    adding jQuery cdn to html, class Die to js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-    
+    class Die {
+        value
+        roll() {
+            return Math.floor(Math.random() * 6)
+        }
+    }
+
+    console.log(roll())
 })
